d3a6f31ed7148d9fa1ba9c519c3dfede327c7bef
Daria <dariapetrov1807@gmail.com>
2025-09-23 22:45:59 +0200

    Add files via upload

---
+++ src/pruebaProperties/PruebaConProperties.java
@@ -0,0 +1,64 @@
+package pruebaProperties;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PruebaConProperties {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Se declara como variable final los ficheros a usar
+		final String fich1 = "archivoPrueba.txt";
+		final String fich2 = "archivoPrueba.xml";
+
+		// Se crean objetos de Properties y FileOutputStreams para guardar
+		// en dos ficheros los mismos datos
+		Properties ejemploGuardar = new Properties();
+		
+		FileOutputStream outputStream = new FileOutputStream(fich1);
+		FileOutputStream outputStream2 = new FileOutputStream(fich2);
+		
+		ejemploGuardar.setProperty("id1", "Ash Ketchum");
+		ejemploGuardar.setProperty("id2", "Pedrito");
+		ejemploGuardar.setProperty("id3", "Carlitos");
+		ejemploGuardar.setProperty("id4", "Ariana Grande");
+		ejemploGuardar.setProperty("id5", "LeBron James");
+		ejemploGuardar.setProperty("id6", "Spike Spiegel");
+		ejemploGuardar.setProperty("id7", "Gerard Way");
+		
+		ejemploGuardar.store(outputStream, "Comentarios");
+		ejemploGuardar.storeToXML(outputStream2, "Comentarios2");
+		
+		// Se crean objetos para leer los archivos de texto
+		
+		Properties ejemploCargar = new Properties();
+		Properties ejemploCargar2 = new Properties();
+		
+		FileInputStream inputStream = new FileInputStream(fich1);
+		FileInputStream inputStream2 = new FileInputStream(fich2);
+		
+		ejemploCargar.load(inputStream);
+		ejemploCargar2.loadFromXML(inputStream2);
+		
+		//Tratamiento del fichero plano .txt
+		//Se accede a un ID en concreto
+		System.out.println("Valor en ID4 en el fichero plano: " + ejemploCargar.getProperty("id4"));
+		
+		//Se leen todos los valores
+		System.out.println(ejemploCargar.toString());
+		
+		//Tratamiento del fichero plano .txt
+		//Se accede a un ID en concreto
+		
+		System.out.println("\nValor en ID6 en el fichero XML: " + ejemploCargar2.getProperty("id6"));
+		
+		//Se leen todos los valores
+		System.out.println(ejemploCargar2.toString());
+		
+		
+		
+	}
+
+}
---
+++ src/pruebaSerializacion/PruebaSerializacion.java
@@ -0,0 +1,62 @@
+package pruebaSerializacion;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PruebaSerializacion {
+
+	public static void main(String[] args) {
+		List<Pokemon> pokedex = new ArrayList<Pokemon>();
+		pokedex.add(new Pokemon("Reshiram", 644, "Dragon"));
+		pokedex.add(new Pokemon("Zekrom", 645, "Dragon"));
+		pokedex.add(new Pokemon("Landorus", 646, "Tierra"));
+		pokedex.add(new Pokemon("Kyurem", 647, "Dragon"));
+		pokedex.add(new Pokemon("Victini", 492, "Fuego"));
+
+		File ficheroPoke = new File("Pokemon.obj");
+ 
+		try {
+			guardarFichero(pokedex, ficheroPoke);
+			leerFichero(ficheroPoke);
+		} catch (IOException e) {
+			System.out.println("error fichero objetos");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error lectura fichero objetos");
+		}
+
+	}
+
+	public static void guardarFichero(List<Pokemon> pokedex, File fichero) throws IOException {
+		FileOutputStream fo = new FileOutputStream(fichero);
+		ObjectOutputStream guardarObjetos = new ObjectOutputStream(fo);
+
+		for (Pokemon p : pokedex) {
+			guardarObjetos.writeObject(p);
+		}
+		System.out.println("Pokedex guardada");
+		guardarObjetos.close();
+	}
+
+	public static void leerFichero(File fichero) throws IOException, ClassNotFoundException {
+		FileInputStream fi = new FileInputStream(fichero);
+		ObjectInputStream datos = new ObjectInputStream(fi);
+		Pokemon p;
+		try {
+			while (true) {
+				p = (Pokemon) datos.readObject();
+				System.out.println(p.getNombre() + " " + p.getNumPokedex() + " " + p.getTipoPrincipal());
+			}
+		} catch (EOFException e) {
+			System.out.println("Fin fichero objetos");
+		}
+		datos.close();
+	}
+
+}
